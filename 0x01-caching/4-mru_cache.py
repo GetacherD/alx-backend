3d750f8bc40286ca027aa4317d8fa8869e84f978
GetacherD <getacher.d@gmail.com>
2023-01-24 20:56:07 +0300

    first

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -11,18 +11,23 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """ initialize """
         super().__init__()
+        self.discard = None
 
     def put(self, key, item):
         """ put key to a value """
+        if len(self.cache_data) == self.MAX_ITEMS and (
+                key and item and key not in self.cache_data):
+            del self.cache_data[self.discard]
+            print("DISCARD:", self.discard)
         if key and item:
             self.cache_data[key] = item
-            discard = key
-            if len(self.cache_data) > self.MAX_ITEMS:
-                del self.cache_data[discard]
-                print("DISCARD:", discard)
+            self.discard = key
 
     def get(self, key):
         """ get cached data """
         if not key:
             return None
-        return self.cache_data.get(key)
+        val = self.cache_data.get(key)
+        if val:
+            self.discard = key
+        return val
